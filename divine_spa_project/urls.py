21d81c40898732588a345e0f6d373b541b9c0156
Candyce10 <candycematthews@yahoo.com>
2022-06-03 12:36:53 -0700

    fixed django files

--- divine_spa_project/urls.py
+++ divine_spa_project/urls.py
@@ -1,4 +1,4 @@
-"""divine_spa URL Configuration
+"""divine_spa_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
