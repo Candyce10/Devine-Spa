7e70b10476c6f2de1a92420783673e0bb5c86baa
Candyce10 <candycematthews@yahoo.com>
2022-06-05 12:39:31 -0700

    added nav and bootstrap cards created

--- main_app/views.py
+++ main_app/views.py
@@ -30,7 +30,9 @@ class Service:
         self.packageprice3 = packageprice3
 
 services = [
-    Service("Massage", "https://media.allure.com/photos/5ee11520a9ba330008e32528/4:3/w_2664,h_1998,c_limit/massage.jpg", "nice massage", "30 Minutes", "$40", "60minutes", "$50", "90 minutes", "$60")
+    Service("Massage", "https://media.allure.com/photos/5ee11520a9ba330008e32528/4:3/w_2664,h_1998,c_limit/massage.jpg", "nice massage", "30 Minutes", "$40", "60minutes", "$50", "90 minutes", "$60"),
+    Service("Facials", "https://media.allure.com/photos/5ee11520a9ba330008e32528/4:3/w_2664,h_1998,c_limit/massage.jpg", "nice facial", "30 Minutes", "$40", "60minutes", "$50", "90 minutes", "$60"),
+    Service("Packages", "https://media.allure.com/photos/5ee11520a9ba330008e32528/4:3/w_2664,h_1998,c_limit/massage.jpg", "nice package", "30 Minutes", "$40", "60minutes", "$50", "90 minutes", "$60")
 ]
        
 class ServiceList(TemplateView):
