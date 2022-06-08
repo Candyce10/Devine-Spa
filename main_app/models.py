cb002d9d78c4cf34b520e8d3fc0ecaec41c430cb
Candyce10 <candycematthews@yahoo.com>
2022-06-08 11:02:24 -0700

    added date to appointment form

--- main_app/models.py
+++ main_app/models.py
@@ -23,6 +23,7 @@ class Appointment(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=150)
     number = models.CharField(max_length=15)
+    date = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
