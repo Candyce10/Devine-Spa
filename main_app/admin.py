8e1d32afff134787788a1cc94fb5ad41020ac45a
Candyce10 <candycematthews@yahoo.com>
2022-06-11 15:58:23 -0700

    added back detail database

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Service, Review, Appointment, Contact
+from .models import Service, Review, Appointment, Contact, ServiceDetails
 
 admin.site.register(Service)
+admin.site.register(ServiceDetails)
+
 admin.site.register(Review)
 admin.site.register(Appointment)
 admin.site.register(Contact)
