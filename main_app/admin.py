56fd6da66e047708822ab653293b9a65ebe92c25
Candyce10 <candycematthews@yahoo.com>
2022-06-11 16:53:50 -0700

    updated carousel

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,7 +3,6 @@ from .models import Service, Review, Appointment, Contact, ServiceDetails
 
 admin.site.register(Service)
 admin.site.register(ServiceDetails)
-
 admin.site.register(Review)
 admin.site.register(Appointment)
 admin.site.register(Contact)
--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,4 @@
 from django.forms import ModelForm
-
-
 from .models import Review
 
 
